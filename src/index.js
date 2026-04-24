52d0c61cff64ec3bbfebae012e018e1e05828b82
hyeonju.lee <hj.lee@interxlab.com>
2026-04-24 15:44:20 +0900

    fix: publish

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './App.css';
 
-export { DataGrid, DEFAULT_ROW_SELECTION, COLUMN_SIZE_MODE } from './grid/components/DataGrid';
-export { TreeDataGrid } from './grid/components/TreeDataGrid';
-export { default as IXGrid } from './grid/components/IXGrid';
+export { DataGrid, DEFAULT_ROW_SELECTION, COLUMN_SIZE_MODE } from './grid/components/DataGrid.jsx';
+export { TreeDataGrid } from './grid/components/TreeDataGrid.jsx';
+export { default as IXGrid } from './grid/components/IXGrid.jsx';
+export { default } from './grid/components/IXGrid.jsx';
